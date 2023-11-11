369152072e27a57b1edbbc2f32530bc26a423c7e
A.M. Nolan <11098245+amnolan@users.noreply.github.com>
2023-11-11 15:57:06 -0600

    Update sample_covariance.py WIP

--- sample_covariance.py
+++ sample_covariance.py
@@ -1,4 +1,3 @@
-#work in progress
 def convert_to_float(input_row):
     output_row = []
     for i in input_row:
@@ -13,13 +12,22 @@ def two_d_deep_copy(src_array):
             sub_ra.append(src_array[i][j])
         dest_array.append(sub_ra)
     return dest_array
-# def pretty_print(ra):
-#     for x, item in enumerate(ra):
-#         if x == len(ra)-1:
-#             str(item)
-#         else:
-#             str(item)+ " "
 
+def total_up_mean_matrix(ra):
+    totals_list = []
+    for i in range(0,len(ra)):
+        total = 0
+        for j in range(0,len(ra[i])):
+            total += ra[i][j]
+        totals_list.append(total)
+    return totals_list
+
+def get_x_hat_array(ra,mean_vector):
+    #iterate over vectors
+    for i in range(0,len(ra[0])):
+        ra[0][i] = ra[0][i] - mean_vector[0]
+        ra[1][i] = ra[1][i] - mean_vector[1]
+    return ra
 print("Convers Mtrx of obsvsn to mean-deviation form")
 print("Given matrix A create M=(1/N)[X_1,X_2...X_N]")
 print("Input top row of matrix comma separated")
@@ -30,18 +38,25 @@ parent_ra = []
 copy_parent_ra = []
 parent_ra.append(top_row)
 parent_ra.append(bottom_row)
-#print(parent_ra)
-n = len(parent_ra[0])
-print(n)
 
-copy_parent_ra = two_d_deep_copy(parent_ra)
+n = len(parent_ra[0])
+print("Size (n):",n)
+
+original_ra = two_d_deep_copy(parent_ra)
 print(parent_ra)
-print(copy_parent_ra)
+print(original_ra)
 
 # iterate over each vector
-# for count in range(0,n):
-#     parent_ra[0][count] = (1/n)*parent_ra[0][count]
-#     parent_ra[1][count] = (1/n)*parent_ra[1][count]
+for count in range(0,n):
+    parent_ra[0][count] = (1/n)*parent_ra[0][count]
+    parent_ra[1][count] = (1/n)*parent_ra[1][count]
+print("Sample Mean Matrix: [top, bottom] respectively")
+sample_mean_mtrx = total_up_mean_matrix(parent_ra)
+print(sample_mean_mtrx)
+print("Press any key to proceed:")
+input()
+print("Finding Xhat for each vector")
 
-# print(parent_ra)
-# print(copy_parent_ra)
+x_hat_ra = two_d_deep_copy(parent_ra)
+x_hat_ra = get_x_hat_array(x_hat_ra,sample_mean_mtrx)
+print(x_hat_ra)
